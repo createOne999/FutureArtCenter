5fb17849eb391f5fb5b6552f3c61963c3f2902bd
createOne999 <castleOne999@gmail.com>
2023-01-20 14:25:55 +0900

    application.properties 설정보완, mapperscan추가, mybatis-config 변경

--- src/main/java/com/futureArtCenter/FutureArtCenterApplication.java
+++ src/main/java/com/futureArtCenter/FutureArtCenterApplication.java
@@ -1,9 +1,12 @@
 package com.futureArtCenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Mapper 인터페이스 스캔 활성화
 @SpringBootApplication
+@MapperScan(basePackages = "com.futureArtCenter.**.**.dao")
 public class FutureArtCenterApplication {
 
 	public static void main(String[] args) {
